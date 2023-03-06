9dc4e5c4472b175bcfc18b5daeb7d6008032420c
krishnamurthypradeep <pradeeepmurthy@gmail.com>
2023-03-06 12:54:31 +0530

    Initial Commit

---
+++ src/main/groovy/com/myapp/spring/DevopsProductsApplication.groovy
@@ -0,0 +1,13 @@
+package com.myapp.spring
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DevopsProductsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DevopsProductsApplication, args)
+	}
+
+}
